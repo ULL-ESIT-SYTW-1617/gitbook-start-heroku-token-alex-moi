fc08cd1fd38d113692c8433169d5dba57c5b0c0a
alu0100767421 <alu0100767421@ull.edu.es>
2016-10-30 12:39:23 +0000

    modificando initialize

--- gitbook-start-heroku.js
+++ gitbook-start-heroku.js
@@ -3,7 +3,7 @@ var exec = require("ssh-exec");
 var fs = require('fs');
 var path = require('path');
 const GitUrlParse = require("git-url-parse");
-
+var child = require("child_process");
 
 
 function initialize(directorio) {
@@ -12,10 +12,8 @@ function initialize(directorio) {
     var contenido='\ngulp.task("deploy-heroku", function () {'+ 
         '\n\tvar heroku = require("gitbook-start-heroku-alex-moi");'+
         '\n\tvar url = paquete.repository.url;'+
-        '\n\tvar iaas_ip = paquete.iaas.IP;'+
-        '\n\tvar iaas_path = paquete.iaas.PATH;'+
         
-        '\n\n\ heroku.deploy(iaas_ip, iaas_path, url);'+
+        '\n\n\ heroku.deploy();'+
         '\n});\n\n';
 
     
@@ -45,43 +43,57 @@ function initialize(directorio) {
           console.log(err);
          console.log("Añadiendo archivo Procfile");
     });
-
+    
+     fs.copyFileSync(path.join(process.cwd(), 'node_modules','gitbook-start-alex-moi-nitesh','template','.env'), path.join(process.cwd(), directorio , '.env'),function(err){
+        if(err)
+          console.log(err);
+      });
+    
+    
+     child.exec('heroku login', function(error, stdout, stderr){
+        if(error)
+          console.log(error);
+        
+        console.log(stderr);
+        console.log(stdout);
+      });
+        
+        
+    var comprobar=false;
+    while(comprobar == false){
+        var nombre_app;
+        nombre_app=prompt('Ingrese su nombre para su aplicacion:','');
+        child.exec('heroku create' + nombre_app, function(err){
+            if(err)
+            console.log("debe introducir un nombre nuevo");
+            else{
+                console.log("aplicacion creada");
+                comprobar=true;
+            }
+        });
+    }
+    
 };
 
-function deploy(ip, ruta, url) {
+function deploy() {
 
-    var carpeta = GitUrlParse(url);
-
-    console.log("Comenzando el deploy en Iaas");
-    console.log('Direccion IP Destino: '+ip);
-    console.log('Ruta de destino: '+ruta+'/'+carpeta.name);
-    console.log('Repositorio origen: '+url);
     
 
-
-    exec('cd '+ruta+';git clone '+url+'',{
-          user: 'usuario',
-          host: ip,
-          key: 'fs.readFileSync(`${process.env.HOME}/.ssh/id_rsa`)'
+    console.log("Comenzando el deploy en HEROKU");
+   
     
-      },function(err){
-       if(err){
-      	console.log('Haciendo pull del repositorio!');
-        exec('cd '+ruta+'/'+carpeta.name+'; git pull',{
-            user: 'usuario',
-            host: ip,
-            key: 'fs.readFileSync(`${process.env.HOME}/.ssh/id_rsa`)'
-          },function(err){ 
-            if(err)
-                console.log("Ha habido un error con el pull");
-            else
-                console.log("Actualizacion carpeta confirmada");
-            });
-        }
-        else {
-            console.log("Clonación del repositorio confirmada");
-        }
-    });
+    
+    child.exec('git add . ; git commit -m "subiendo a heroku"; git push heroku master;', function(error, stdout, stderr){
+        if(error)
+          console.log(error)
+        
+        console.log(stderr);
+        console.log(stdout);
+      })
+
+    
+
+   
 };
 
 module.exports = {
